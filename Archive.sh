2db5d3f24edfb9d43450f642bc609b1129fe8731
minecraftitsover90@gmail.com <Connor Jacobs>
2023-06-01 13:23:16 +0930

    Created Repo and Added Archive Script/Workflow

---
+++ Archive.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Function to compare Etag values
+compare_etag() {
+    local url=$1
+    local etag=$2
+    local stored_etag=$(jq -r ".$url" Etag.json)
+
+    if [[ "$etag" != "$stored_etag" ]]; then
+        echo "Downloading $url..."
+        curl -sI $url | grep -i "etag" | awk -F'"' '{print $2}' | jq -R '.' | jq -c -r ". as \$val | {\"$url\": \$val}" >> Etag.json
+        curl -s -O $url
+    else
+        echo "No update available for $url"
+    fi
+}
+
+# List of URLs to download
+urls=(
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/athenamessage"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/battlepassaboutmessages"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/battlepasspurchase"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/battleroyalenews"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/battleroyalenewsv2"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/comics"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/creative-ads"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/creative-features"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/creativenews"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/creativenewsv2"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/crewscreendata"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/dynamicbackgrounds"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/emergencynotice"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/emergencynoticev2"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/eventscreens"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/koreancafe"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/leaderboardinformation"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/lobby"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/loginmessage"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/media-events"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/media-events-v2"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/platformpurchasemessaging"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/playersurvey"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/playersurveyv2"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/playlistinformation"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/radio-stations"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/savetheworldnews"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/scoringrulesinformation"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-carousel"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-sections"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/socialevents"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/specialoffervideo"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/subgameinfo"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/subgameselectdata"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/subscription"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/survivalmessage"
+    "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/tournamentinformation"
+)
+
+# Iterate through the URLs
+for url in "${urls[@]}"; do
+    etag=$(curl -sI $url | grep -i "etag" | awk -F'"' '{print $2}')
+    if [[ ! -f "Etag.json" ]]; then
+        echo "{}" > Etag.json
+    fi
+    compare_etag $url $etag
+done
---
+++ README.md
@@ -0,0 +1,3 @@
+#Fortnite-Content Archive
+
+Archiving https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game and all respective pages
